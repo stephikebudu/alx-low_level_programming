d0f73477771d3f856659368501922e333722e3e8
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-16 14:44:03 -0800

    used puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this displays some text
+ *
+ * Return: this is necessary syntax
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
