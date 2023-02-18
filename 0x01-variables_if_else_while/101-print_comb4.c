b5330293f2792d2b9ff86866585c26c692da53c6
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-18 07:34:04 -0800

    done task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,20 +16,17 @@ int main(void)
 	{
 		for (j = 1; j < 9; j++)
 		{
-			if (i < j && i != j)
+			for (k = 2; k < 10; k++)
 			{
-				for (k = 2; k < 10; k++)
+				if (i < j && j < k && i != j && j != k)
 				{
-					if (k < j && k != j)
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i * k != 63)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(k + '0');
-						if (i * k != 63)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
