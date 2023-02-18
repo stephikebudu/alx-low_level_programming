ead3aefb87f49f58fc32d62ab9bddaf6ddc620c8
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-17 17:03:03 -0800

    completed task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
