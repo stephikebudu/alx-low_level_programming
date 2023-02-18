4be6c18a652ac6cdc7887da97798faa32860f88c
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-17 17:29:02 -0800

    completed task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - displays base 16 numbers in lower case
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 0; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
