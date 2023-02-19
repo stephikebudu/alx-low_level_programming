1840e6ad47f6485c8b69cb77567711e0f9592793
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-19 13:30:48 -0800

    complete advanced task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints a quote by Dora Korpar
+ *
+ * Return: returns 1
+ */
+
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
