3a5dbae472cd2c7f22bac467874b89f73a7f91e7
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-21 19:07:42 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function adds two numbers
+ *
+ * @i: function parameter 1
+ * @j: function parameter 2
+ *
+ * Return: sum of i & j on success
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function prints 9 times table
+ *
+ * Return: 0 on success
+ */
+
+void times_table(void)
+{
+
+}
