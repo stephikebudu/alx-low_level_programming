ab17e5a7511c12cf2c10da81d629403c4cc20d0d
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-21 20:43:55 -0800

    correct task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,5 +8,22 @@
 
 void times_table(void)
 {
+	int i;
+	int j;
+	int num;
 
+	for (i = 0; i  < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			num = i * j;
+			_putchar(num + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
