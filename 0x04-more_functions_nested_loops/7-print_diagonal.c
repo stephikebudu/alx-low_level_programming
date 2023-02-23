638d76c964697c4f60bcc1f36a58bc486d1a76e5
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-23 13:30:35 -0800

    correct function that prints diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,25 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
+		int j;
+		int k;
+
 		if (n <= 0)
 		{
 			_putchar('\n');
 		}
+		else if (n == 1)
+		{
+			_putchar('\\');
+		}
 		else
 		{
+			j = n - 1;
+
+			for (k = 1; k <= j; k++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 		}
 	}
