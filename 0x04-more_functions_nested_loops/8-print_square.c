17ff6a7023c4276ea2f6ed6818ec7bc67d4aa196
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-23 13:41:17 -0800

    create function that prints square with #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints square to standard output with #.
+ * @size: function parameter.
+ * Return: 0 on success
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
