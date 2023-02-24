b9a04629c7de0d068a40dc4d4c67c96fb721627c
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-23 17:36:55 -0800

    create function to print triangle with #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - function prints triangle on standard output.
+ * @size: function parameter.
+ * Return: 0 on success.
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
