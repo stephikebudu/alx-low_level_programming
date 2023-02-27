bfa8e6aba2370012242beff510489e5ee7e34893
Stephany Ikebudu <stephikebudu@gmail.com>
2023-02-27 14:00:52 -0800

    reset pointer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function resets parameter value to 98.
+ *
+ * @n: function parameter.
+ *
+ * Return: 0on success.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
