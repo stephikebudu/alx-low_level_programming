cedffec40d233a979ccaa4fe48941e152097349a
Stephany Ikebudu <stephikebudu@gmail.com>
2023-03-06 09:11:15 -0800

    create fxn that 1. copies memory area & 2. locates char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - entry point
+ * @s: function parameter 1.
+ * @c: function parameter 2.
+ *
+ * Return: 0 on success.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return(&s[i]);
+	}
+	return (0);
+}
