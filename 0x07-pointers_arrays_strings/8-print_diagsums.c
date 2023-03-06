a9e04388ab3d27a31707e663fa82b0a17774b326
Stephany Ikebudu <stephikebudu@gmail.com>
2023-03-06 10:10:36 -0800

    create fxn
    8. fxn prints diagonal sum of two diagonals of square matrix
    100. fxn sets pointer value to char
    101. file contains password

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function that printssum of diagonal in square matrix.
+ * @a: function parameter 1.
+ * @size: function parameter 2.
+ *
+ * Return: 0 on success.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1;
+	int sum2;
+	int y;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum1 + a[y * size + y];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
