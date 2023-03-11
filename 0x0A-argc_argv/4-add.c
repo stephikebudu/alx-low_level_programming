87b6152b9427cce3f74e085439950f95beb99b64
Stephany Ikebudu <stephikebudu@gmail.com>
2023-03-10 18:06:18 -0800

    create program to add positive no.s

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string for digit
+ * @str: array string
+ *
+ * Return: Always 0 on success
+ */
+
+int check_num(char *str)
+{
+	/* declare variables */
+	unsigned int count;
+
+	count = 0;
+
+	while (count < strlen(str)) /* count string */
+	{
+		if (!isdigit(str[count])) /* check if str is digit */
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Always 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	/* declare variables */
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+
+	while (count < argc) /* checks the whole array */
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /* atoi program converts string to integer */
+			sum += str_to_int;
+		}
+		else /* condition in case one of the numbers contains a symbol that is not a digit */
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum); /* print sum */
+	return (0);
+}
